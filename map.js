ec5931922770a6c2932d507894b29aae345fccbc
Moawiya <111635333+Moawiya-Sajjad@users.noreply.github.com>
2023-10-10 11:48:29 +0800

    Upload files

---
+++ map.js
@@ -0,0 +1,5 @@
+var map_1 = "map.vg.json";
+var bubbleplot_1 = "bubbleplot.vg.json";
+
+vegaEmbed("#map", map_1).then(function (result) {}).catch(console.error);
+vegaEmbed("#bubbleplot", bubbleplot_1).then(function (result) {}).catch(console.error);
